099990fc9169e0d431774248677747cc8fa93627
昊色居士 <xhrtxh@gmail.com>
2023-03-11 14:15:48 +0800

    💅 style: 代码格式化

--- sanic_api/logger/extend.py
+++ sanic_api/logger/extend.py
@@ -9,9 +9,9 @@ from loguru import logger
 from loguru._defaults import env
 from sanic import HTTPResponse, Request
 from sanic.server import HttpProtocol
-from sanic_api.enum import RunModeEnum
 from sanic_ext import Extension
 
+from sanic_api.enum import RunModeEnum
 from sanic_api.logger.config import InterceptHandler
 from sanic_api.logger.sanic_http import SanicHttp
 
@@ -27,7 +27,7 @@ class LoggerExtend(Extension):
         if not self.included():
             return
 
-        is_debug = self.app.config.get("mode") in [RunModeEnum.DEBUG, RunModeEnum.DEV],
+        is_debug = (self.app.config.get("mode") in [RunModeEnum.DEBUG, RunModeEnum.DEV],)
         log_level = logging.DEBUG if is_debug else logging.INFO
         log_format = env(
             "LOGURU_FORMAT",
