5cf81e7820db578fad91086c22c5a2fdb509e648
昊色居士 <xhrtxh@gmail.com>
2023-03-17 15:02:22 +0800

    ➕ improvement(utils)：工具方法中新增自动蓝图方法

--- sanic_api/utils.py
+++ sanic_api/utils.py
@@ -1,10 +1,15 @@
 import os
+import re
 from decimal import Decimal
+from glob import glob
+from importlib import import_module
+from inspect import getmembers
 from pathlib import Path
-from typing import Optional
+from typing import Any, Dict, Optional
 
 import orjson
-from sanic import Request
+from sanic import Blueprint, Request, Sanic
+from sanic.blueprint_group import BlueprintGroup
 from sanic.exceptions import ServerError as SanicServerError
 
 
@@ -54,3 +59,46 @@ def get_current_request() -> Optional[Request]:
         return Request.get_current()
     except SanicServerError:
         return None
+
+
+def auto_blueprint(sanic_app: Sanic, base_api_module_name):
+    """
+    自动生成蓝图
+    Args:
+        sanic_app: app
+        base_api_module_name: api层模块名称
+
+    Returns:
+
+    """
+    # app 名称
+    app_name = Path(os.getcwd()).parent.name
+    # api层名称
+    base_api_name: str = base_api_module_name.split(".")[-1]
+    # api层模块
+    base_api_module = import_module(base_api_module_name, sanic_app.__module__)
+    # api层目录
+    base_api_dir = Path(str(base_api_module.__file__)).parent
+
+    # 所有路由组的字典
+    blueprint_group: Dict[str, BlueprintGroup] = {}
+
+    # 遍历所有__init__文件找到所有蓝图
+    for path in glob(f"{base_api_dir}/**/__init__.py", recursive=True):
+        # 蓝图所在上层的模块
+        modules = re.findall(rf"{app_name}/(.+?)/__", path)[0].split("/")
+        specmod = import_module(".".join(modules), sanic_app.__module__)
+
+        # 获取该模块下的所有蓝图
+        blueprints = [m[1] for m in getmembers(specmod, lambda o: isinstance(o, Blueprint))]
+
+        blueprint_modules = modules[::-1][1:-1] if modules[-1] != "api" else modules[::-1][:-1]
+        for index, m in enumerate(blueprint_modules):
+            blueprint_group[m] = blueprint_group.get(m, Blueprint.group(url_prefix=m))
+            if index == 0:
+                blueprint_group[m].extend(blueprints)
+            else:
+                prev_bg: Any = blueprint_group.get(blueprint_modules[index - 1])
+                blueprint_group[m].append(prev_bg)
+
+    sanic_app.blueprint(blueprint_group[base_api_name])
